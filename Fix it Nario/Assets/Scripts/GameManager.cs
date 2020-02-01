e97a89abde7fb88c36167045c8b0fb0e36e29a21
Tensai <jonathan.bengoa@gmail.com>
2020-02-01 11:34:13 -0300

    add prototype objects

---
+++ Fix it Nario/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlatformerGO;
+    public GameObject FixerGO;
+
+    bool isGameInPause = false;
+
+    private MovimientoPlayer1 playerM;
+    private FixerManager fixerM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate Platformer and Fixer
+        fixerM = Instantiate(FixerGO, Vector3.zero, Quaternion.identity).GetComponent<FixerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameInPause)
+        {
+            fixerM.UpdateFixerManager();
+        }
+    }
+}
