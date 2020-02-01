553d68e6c3e8b4fed6589627c714aeacadc62d28
maxmouser <bolia.maximiliano@gmail.com>
2020-02-01 20:04:24 -0300

    Se agrega muerte del player con el piso en la escena de platform

--- Fix it Nario/Assets/Scripts/MovimientoPlayer1.cs
+++ Fix it Nario/Assets/Scripts/MovimientoPlayer1.cs
@@ -19,7 +19,7 @@ public class MovimientoPlayer1 : MonoBehaviour
     public bool isFalling= false;
     public bool isGrounded;
     public bool stairsCollision = false;
-
+    public bool collisionPusherCamera = false;
 
     void Start()
     {
@@ -37,8 +37,6 @@ public class MovimientoPlayer1 : MonoBehaviour
         void Update()
     {
 		//GetComponent<Animation>().Play("Idle");
-
-
     }
 
     public void movimientoHorizontal(){
@@ -46,26 +44,18 @@ public class MovimientoPlayer1 : MonoBehaviour
 
        if(Input.GetKey(KeyCode.D))
         {
-            //this.transform.Translate(speed, 0.0f, 0.0f);
-            //Transform.position = oldPosition;
            this.transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z );
-
         }
 
-       if(Input.GetKey(KeyCode.A))
+       if(Input.GetKey(KeyCode.A) && !collisionPusherCamera)
         {
-            //this.transform.Translate(speed, 0.0f, 0.0f);
-            //Transform.position = oldPosition;
-           this.transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
-
+        	this.transform.Translate(10 * transform.right * Time.deltaTime);
+           //this.transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
         }
 
        	if(Input.GetKey(KeyCode.W) && stairsCollision)
         {
-            //this.transform.Translate(speed, 0.0f, 0.0f);
-            //Transform.position = oldPosition;
            this.transform.position = new Vector3(transform.position.x, transform.position.y+speed * Time.deltaTime, transform.position.z);
-
         }
         //float moveHorizontal = Input.GetAxis("Horizontal");
         //Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0);
@@ -128,6 +118,23 @@ public class MovimientoPlayer1 : MonoBehaviour
             rb.useGravity = false;
         }
     }
+   
+
+    void OnTriggerEnter(Collider other){
+
+         if (other.gameObject.layer == 10){ //Layer 10 es el empujador de la camara
+         	collisionPusherCamera = true;
+              //this.transform.position = new Vector3(transform.position.x + 10 * Time.deltaTime, transform.position.y, transform.position.z);
+              //this.transform.Translate(-20 * transform.right * Time.deltaTime);
+
+         }
+          if (other.gameObject.layer == 11){ //Layer 11 es el el piso de muerte
+				muerte();
+              //this.transform.position = new Vector3(transform.position.x + 10 * Time.deltaTime, transform.position.y, transform.position.z);
+              //this.transform.Translate(-20 * transform.right * Time.deltaTime);
+
+         }
+  	}
 
     void OnTriggerExit(Collider other){
 
@@ -136,10 +143,15 @@ public class MovimientoPlayer1 : MonoBehaviour
             stairsCollision = false;
             rb.useGravity = true;
         }
+        if (other.gameObject.layer == 10){
+
+        	collisionPusherCamera = false;
+        }
     }
 
+    void muerte(){
+    	print("MURIO EL PLAYER");
 
-
-
+    }
 
 }
