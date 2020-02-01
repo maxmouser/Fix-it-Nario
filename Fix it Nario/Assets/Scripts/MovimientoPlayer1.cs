9593db3c6c7b63f1fdf70cd8db15ed5e24e3a7b2
maxmouser <bolia.maximiliano@gmail.com>
2020-02-01 01:50:48 -0300

    Primer Push
    Commit 1:
    - Escena principal, con el boceto del P1.
    - Script mov player ( se debe pasar al platform)
    -Script del platform

---
+++ Fix it Nario/Assets/Scripts/MovimientoPlayer1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPlayer1 : MonoBehaviour
+{
+    public int speed = 10;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        //Vector3 desiredVelocity = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        //rb.velocity = Vector3.Lerp(rb.velocity, desiredVelocity, Time.deltaTime * 5f);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0);
+
+        rb.AddForce(movement * speed);
+    }
+
+
+}
---
+++ Fix it Nario/Assets/Scripts/Platformer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void UpdatePlatformer()
+    {
+        
+    }
+
+    
+}
+ class gameManager 
+{
+    bool pause;
+
+    void update()
+    {
+        if (pause) return;
+
+        //platformer.UpdatePlatformer();
+    }
+}
