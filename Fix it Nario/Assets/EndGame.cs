c77565823cd5166467c52d8dfb714aba7517abd8
Tensai <jonathan.bengoa@gmail.com>
2020-02-02 12:11:07 -0300

    EndGame.cs

---
+++ Fix it Nario/Assets/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer == 8)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+}
