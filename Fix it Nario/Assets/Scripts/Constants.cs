a13a71bd3f219cc5b6a93259f0c2a721326bea60
Tensai <jonathan.bengoa@gmail.com>
2020-02-02 10:15:44 -0300

    divisor

--- Fix it Nario/Assets/Scripts/Constants.cs
+++ Fix it Nario/Assets/Scripts/Constants.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Constants 
+public static class Constants 
 {
     public const int Z_WORLD = 0;
     public const float TIME_SHAKING = 4;
+    public static AudioManager AUDIO_MANAGER;
 }
